26e433caaadd5d0b0d3e0ba43cff7f56faf22e99
Malachi D. Danielewicz <Malachi@cecserver.conrad.com>
2016-05-16 16:58:26 -0400

    Added the ability to freeze selected die/dice from rolling. Started on calculating/displaying score on hover.

--- dice.js
+++ dice.js
@@ -1,4 +1,5 @@
 var dieHTML = '';
+var numbers = [];
 
 
 var dice = {
@@ -12,6 +13,7 @@ var dice = {
     if (this.d1 === true){
         var randomNumber = Math.floor(Math.random()*this.sides)+1;
         var dieHTML = '<h1 style="color: red; text-align: center; margin:30px auto;">'+ randomNumber + '</h1>';
+        numbers.splice(0, 1, randomNumber);
         $('.die-one h1').replaceWith(dieHTML);
         console.log(randomNumber);
         }
@@ -20,6 +22,7 @@ var dice = {
     if (this.d2 === true){
         var randomNumber = Math.floor(Math.random()*this.sides)+1;
         var dieHTML = '<h1 style="color: orange; text-align: center; margin:30px auto;">'+ randomNumber + '</h1>';
+        numbers.splice(1, 1, randomNumber);
         $('.die-two h1').replaceWith(dieHTML);
         console.log(randomNumber);
         }
@@ -28,6 +31,7 @@ var dice = {
     if (this.d3 === true){
         var randomNumber = Math.floor(Math.random()*this.sides)+1;
         var dieHTML = '<h1 style="color: yellow; text-align: center; margin:30px auto;">'+ randomNumber + '</h1>';
+        numbers.splice(2, 1, randomNumber);
         $('.die-three h1').replaceWith(dieHTML);
         console.log(randomNumber);
         }
@@ -36,6 +40,7 @@ var dice = {
     if (this.d4 === true){
         var randomNumber = Math.floor(Math.random()*this.sides)+1;
         var dieHTML = '<h1 style="color: green; text-align: center; margin:30px auto;">'+ randomNumber + '</h1>';
+        numbers.splice(3, 1, randomNumber);
         $('.die-four h1').replaceWith(dieHTML);
         console.log(randomNumber);
         }
@@ -44,19 +49,96 @@ var dice = {
     if (this.d5 === true){
         var randomNumber = Math.floor(Math.random()*this.sides)+1;
         var dieHTML = '<h1 style="color: blue; text-align: center; margin:30px auto;">'+ randomNumber + '</h1>';
+        numbers.splice(4, 1, randomNumber);
         $('.die-five h1').replaceWith(dieHTML);
         console.log(randomNumber);
         }
     } 
 }
 
+var roll = {
+    turn: 0,
+    rollRule: function(){
+        if(roll.turn <5){
+        dice.rollOne();
+        dice.rollTwo();
+        dice.rollThree();
+        dice.rollFour();
+        dice.rollFive();
+        roll.turn ++; 
+        console.log("Number Array: " + numbers)    
+    }else{
+        console.log("Select Score!");
+        }
+    }
+
+}
+
+var score = {
+    ones: function(){
+        var onesTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 1){
+                onesTotal.push(numbers[i]);
+            }
+        }
+        
+        console.log(onesTotal);
+    },
+    twos: function(){
+        var twosTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 2){
+                twosTotal.push(numbers[i]);
+            }
+        }
+        
+        console.log(twosTotal);
+    },
+    threes: function(){
+        var threesTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 3){
+                threesTotal.push(numbers[i]);
+            }
+        }
+        
+        console.log(threesTotal);
+    },
+    fours: function(){
+        var foursTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 4){
+                foursTotal.push(numbers[i]);
+            }
+        }
+        
+        console.log(foursTotal);
+    },
+    fives: function(){
+        var fivesTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 5){
+                fivesTotal.push(numbers[i]);
+            }
+            console.log(fivesTotal);
+        }
+    },
+    sixes: function(){
+        var sixesTotal = [];
+        for (var i = 0; i < numbers.length; i++){
+            if (numbers[i] == 6){
+                sixesTotal.push(numbers[i]);
+            }
+        }
+        
+        console.log(sixesTotal);
+    }
+    
+}
 
 $('.roll').on().click(function() {
-    dice.rollOne();
-    dice.rollTwo();
-    dice.rollThree();
-    dice.rollFour();
-    dice.rollFive();
+    roll.rollRule();
     
 });
 
@@ -94,3 +176,23 @@ $('.die-five').click(function() {
     console.log("d5"+dice.d5);
     console.log("slected die")
 });
+
+$('.ones').on().mouseover(function() {
+    score.ones();
+})
+$('.twos').on().mouseover(function() {
+    score.twos();
+})
+$('.threes').on().mouseover(function() {
+    score.threes();
+})
+$('.fours').on().mouseover(function() {
+    score.fours();
+})
+$('.fives').on().mouseover(function() {
+    score.fives();
+})
+$('.sixes').on().mouseover(function() {
+    score.sixes();
+})
+
